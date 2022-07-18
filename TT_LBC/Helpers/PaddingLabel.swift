d3509dfcb9d6102773cdb77979efd2a7486a286c
Thibaud Lambert <thib.lambert@icloud.com>
2022-07-18 23:25:39 +0200

    [Components] Create PaddingLabel

---
+++ TT_LBC/Helpers/PaddingLabel.swift
@@ -0,0 +1,35 @@
+//
+//  PaddingLabel.swift
+//  TT_LBC
+//
+//  Created by Thibaud Lambert on 18/07/2022.
+//
+
+import Foundation
+import UIKit
+
+class PaddingLabel: UILabel {
+	
+	// MARK: - Inspectables
+	var topInset: CGFloat = 0
+	var bottomInset: CGFloat = 0
+	var leadingInset: CGFloat = 0
+	var traillingInset: CGFloat = 0
+	
+	// MARK: - Variables
+	override open var intrinsicContentSize: CGSize {
+		var intrinsicSuperViewContentSize = super.intrinsicContentSize
+		intrinsicSuperViewContentSize.height += self.topInset + self.bottomInset
+		intrinsicSuperViewContentSize.width += self.leadingInset + self.traillingInset
+		
+		return intrinsicSuperViewContentSize
+	}
+	
+	// MARK: - Helpers
+	func updateAllCorners(with value: CGFloat) {
+		self.topInset = value
+		self.bottomInset = value
+		self.leadingInset = value
+		self.traillingInset = value
+	}
+}
