f69a85e56ec48d466a2043aea32abe9d66a08e00
Thibaud Lambert <thib.lambert@icloud.com>
2022-07-20 18:06:42 +0200

    [Posts] Sort list by ascending date

--- TT_LBC/Helpers/Extensions/Date+String.swift
+++ TT_LBC/Helpers/Extensions/Date+String.swift
@@ -9,7 +9,7 @@ import Foundation
 
 extension Date {
 	
-	func toString(format: String = "dd/MM/yyyy",
+	func toString(format: String = "yyyy-MM-dd'T'HH:mm:ssZ",
 						 locale: Locale = Locale.current) -> String {
 		let formatter = DateFormatter()
 		formatter.locale = locale
--- TT_LBC/Modules/Posts/List/PostsListPresenter.swift
+++ TT_LBC/Modules/Posts/List/PostsListPresenter.swift
@@ -6,14 +6,23 @@
 class PostsListPresenter: Presenter<PostsListViewModel> {
 	
 	func display(posts: [Post], categories: [Category]) {
-		self.viewModel?.items = posts.compactMap { post in
-			PostsListViewModel.Item(id: post.id,
-									title: post.title,
-									image: post.imageUrl,
-									category: categories.first { category in post.categoryId == category.id }?.name ?? "-",
-									price: post.price.toPrice(currency: "€"),
-									isImportant: post.isUrgent)
-		}
+		self.viewModel?.items = posts
+			.sorted {
+				guard
+					let firstDate = $0.creationDate,
+					let secondDate = $1.creationDate
+				else { return false }
+				
+				return firstDate > secondDate
+			}
+			.compactMap { post in
+				PostsListViewModel.Item(id: post.id,
+										title: post.title,
+										image: post.imageUrl,
+										category: categories.first { $0.id == post.categoryId }?.name ?? "-",
+										price: post.price.toPrice(currency: "€"),
+										isImportant: post.isUrgent)
+			}
 		
 		self.viewModel?.send()
 	}
